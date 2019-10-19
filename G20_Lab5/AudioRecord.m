52b8e90aed348b868fc18467a04a12818f7f0af9
DariusPi <darius.piecaitis@mail.mcgill.ca>
2019-10-19 12:08:34 -0400

    completed lab 5

--- G20_Lab5/AudioRecord.m
+++ G20_Lab5/AudioRecord.m
@@ -1,18 +1,41 @@
-% Record your voice for 5 seconds.
-recObj = audiorecorder;
-disp('Start speaking.')
-recordblocking(recObj, 1);
-disp('End of Recording.');
+% % Record your voice for 1 seconds.
+% recObj = audiorecorder;
+% disp('Start speaking.')
+% recordblocking(recObj, 1);
+% disp('End of Recording.');
+% 
+% % Play back the recording.
+% play(recObj);
+% 
+% % Store data in double-precision array.
+% myRecording = getaudiodata(recObj);
+% figure; plot(myRecording); % Plot the original waveform.
+% 
+% % cut the area you want and convert it into integer
+% MyAudioArray = uint16((myRecording(1:8000)+1)*1024/2); 
+% csvwrite('AudioArray.csv',MyAudioArray');
 
-% Play back the recording.
-play(recObj);
+% % Plot the modified waveform.
+% figure;plot(MyAudioArray);
 
-% Store data in double-precision array.
-myRecording = getaudiodata(recObj);
-figure; plot(myRecording); % Plot the original waveform.
+% hexarr=uint8(32000);
+% for j=1:8000
+%     hexarr((j*4))=mod(data(j),(16));
+%     hexarr ((j*4)-1)=mod((idivide(MyAudioArray(j),16)),16);
+%     hexarr ((j*4)-2)=mod((idivide(MyAudioArray(j),256)),16);
+%     hexarr ((j*4)-3)=mod((idivide(MyAudioArray(j),4096)),16);
+%     
+% end
+% 
+% dest = string(dec2hex(hexarr));
 
-% cut the area you want and convert it into integers
-MyAudioArray = uint16((myRecording(1:8000)+1)*2048); 
-csvwrite('AudioArray.csv',MyAudioArray');
-% Plot the modified waveform.
-figure;plot(MyAudioArray);  
+if ~isempty(instrfind)
+     fclose(instrfind);
+      delete(instrfind);
+end
+s1=serial('COM3','BaudRate',115200);
+fopen(s1);
+ 
+for i=1:32000
+    fprintf(s1, '%s',(dest(i)));
+end
