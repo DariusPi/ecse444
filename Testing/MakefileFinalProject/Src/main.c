148768744c153390cabce625d8ecbce243bb16bf
Murko\Murray K <murray.kornelsen@mail.mcgill.ca>
2019-11-19 23:51:24 -0500

    remove rtos

--- Testing/MakefileFinalProject/Src/main.c
+++ Testing/MakefileFinalProject/Src/main.c
@@ -1,26 +1,25 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -29,7 +28,6 @@
 #include "arm_math.h"
 #include "stm32l475e_iot01.h"
 #include "stm32l475e_iot01_qspi.h"
-
 #include "fast_ica.h"
 #include "sine_gen.h"
 // #include <errno.h>
@@ -56,11 +54,12 @@ DAC_HandleTypeDef hdac1;
 
 QSPI_HandleTypeDef hqspi;
 
+RNG_HandleTypeDef hrng;
+
 TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart1;
 
-osThreadId playSoundTaskHandle;
 /* USER CODE BEGIN PV */
 volatile int tim3flag = 0;
 /* USER CODE END PV */
@@ -72,8 +71,7 @@ static void MX_DAC1_Init(void);
 static void MX_QUADSPI_Init(void);
 static void MX_TIM3_Init(void);
 static void MX_USART1_UART_Init(void);
-void StartDefaultTask(void const *argument);
-
+static void MX_RNG_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -81,12 +79,14 @@ void StartDefaultTask(void const *argument);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 int _write(int fd, char *ptr, int len) {
-    while (HAL_OK != HAL_UART_Transmit(&huart1, (uint8_t *)ptr, len, HAL_UART_TIMEOUT_VALUE));
+    while (HAL_OK != HAL_UART_Transmit(&huart1, (uint8_t *)ptr, len, HAL_UART_TIMEOUT_VALUE))
+        ;
     return (len);
 }
 
 int _read(int fd, char *ptr, int len) {
-    while (HAL_OK != HAL_UART_Receive(&huart1, (uint8_t *) ptr, 1, HAL_UART_TIMEOUT_VALUE));
+    while (HAL_OK != HAL_UART_Receive(&huart1, (uint8_t *)ptr, 1, HAL_UART_TIMEOUT_VALUE))
+        ;
     return 1;
 }
 
@@ -104,7 +104,8 @@ void zeroDACValue() {
     int sv1 = HAL_DAC_GetValue(&hdac1, DAC_CHANNEL_1);
     int sv2 = HAL_DAC_GetValue(&hdac1, DAC_CHANNEL_2);
     for (int i = 0; i < sv1 || i < sv2; i++) {
-        while (tim3flag == 0);
+        while (tim3flag == 0)
+            ;
         HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, (sv1 - i) > 0 ? sv1 - i : 0);
         HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_2, DAC_ALIGN_12B_R, (sv2 - i) > 0 ? sv2 - i : 0);
         tim3flag = 0;
@@ -115,9 +116,9 @@ void zeroDACValue() {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void) {
     /* USER CODE BEGIN 1 */
 
@@ -144,45 +145,31 @@ int main(void) {
     MX_DAC1_Init();
     MX_TIM3_Init();
     MX_USART1_UART_Init();
+    MX_RNG_Init();
     /* USER CODE BEGIN 2 */
     HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
     HAL_DAC_Start(&hdac1, DAC_CHANNEL_2);
     HAL_TIM_Base_Start_IT(&htim3);
 
-    SineWave wav1 = {
-        .amplitude = 0.9,
-        .freq = 440.0,
-        .base_addr = 0 * MX25R6435F_BLOCK_SIZE};
+    uint32_t rand;
+    for (int i = 0; i < 20; i++) {
+        HAL_RNG_GenerateRandomNumber(&hrng, &rand);
+        printf("%lu\n", rand);
+    }
 
-    SineWave wav2 = {
-        .amplitude = 0.9,
-        .freq = 261.23,
-        .base_addr = 4 * MX25R6435F_BLOCK_SIZE};
+    SineWave wav1 = {.amplitude = 0.9, .freq = 440.0, .base_addr = 0 * MX25R6435F_BLOCK_SIZE};
 
-    SineWave wav3 = {
-        .amplitude = 0.9,
-        .freq = 392,
-        .base_addr = 8 * MX25R6435F_BLOCK_SIZE};
+    SineWave wav2 = {.amplitude = 0.9, .freq = 261.23, .base_addr = 4 * MX25R6435F_BLOCK_SIZE};
 
-    SineWave mixL = {
-        .amplitude = 1,
-        .freq = 0,
-        .base_addr = 12 * MX25R6435F_BLOCK_SIZE};
+    SineWave wav3 = {.amplitude = 0.9, .freq = 392, .base_addr = 8 * MX25R6435F_BLOCK_SIZE};
 
-    SineWave mixR = {
-        .amplitude = 1,
-        .freq = 0,
-        .base_addr = 16 * MX25R6435F_BLOCK_SIZE};
+    SineWave mixL = {.amplitude = 1, .freq = 0, .base_addr = 12 * MX25R6435F_BLOCK_SIZE};
 
-    SineWave unmixL = {
-        .amplitude = 1,
-        .freq = 0,
-        .base_addr = 24 * MX25R6435F_BLOCK_SIZE};
+    SineWave mixR = {.amplitude = 1, .freq = 0, .base_addr = 16 * MX25R6435F_BLOCK_SIZE};
 
-    SineWave unmixR = {
-        .amplitude = 1,
-        .freq = 0,
-        .base_addr = 28 * MX25R6435F_BLOCK_SIZE};
+    SineWave unmixL = {.amplitude = 1, .freq = 0, .base_addr = 24 * MX25R6435F_BLOCK_SIZE};
+
+    SineWave unmixR = {.amplitude = 1, .freq = 0, .base_addr = 28 * MX25R6435F_BLOCK_SIZE};
 
     sine_gen_init();
 
@@ -219,36 +206,6 @@ int main(void) {
 
     /* USER CODE END 2 */
 
-    /* USER CODE BEGIN RTOS_MUTEX */
-    /* add mutexes, ... */
-    /* USER CODE END RTOS_MUTEX */
-
-    /* USER CODE BEGIN RTOS_SEMAPHORES */
-    /* add semaphores, ... */
-    /* USER CODE END RTOS_SEMAPHORES */
-
-    /* USER CODE BEGIN RTOS_TIMERS */
-    /* start timers, add new ones, ... */
-    /* USER CODE END RTOS_TIMERS */
-
-    /* USER CODE BEGIN RTOS_QUEUES */
-    /* add queues, ... */
-    /* USER CODE END RTOS_QUEUES */
-
-    /* Create the thread(s) */
-    /* definition and creation of playSoundTask */
-    osThreadDef(playSoundTask, StartDefaultTask, osPriorityNormal, 0, 128);
-    playSoundTaskHandle = osThreadCreate(osThread(playSoundTask), NULL);
-
-    /* USER CODE BEGIN RTOS_THREADS */
-    /* add threads, ... */
-    /* USER CODE END RTOS_THREADS */
-
-    /* Start scheduler */
-    osKernelStart();
-
-    /* We should never get here as control is now taken by the scheduler */
-
     /* Infinite loop */
     /* USER CODE BEGIN WHILE */
 
@@ -261,16 +218,16 @@ int main(void) {
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void) {
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
     RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-    /** Initializes the CPU, AHB and APB busses clocks 
-  */
+    /** Initializes the CPU, AHB and APB busses clocks
+     */
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
     RCC_OscInitStruct.MSIState = RCC_MSI_ON;
     RCC_OscInitStruct.MSICalibrationValue = 0;
@@ -285,8 +242,8 @@ void SystemClock_Config(void) {
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
         Error_Handler();
     }
-    /** Initializes the CPU, AHB and APB busses clocks 
-  */
+    /** Initializes the CPU, AHB and APB busses clocks
+     */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
@@ -296,23 +253,31 @@ void SystemClock_Config(void) {
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
         Error_Handler();
     }
-    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_RNG;
     PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
+    PeriphClkInit.RngClockSelection = RCC_RNGCLKSOURCE_PLLSAI1;
+    PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_MSI;
+    PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
+    PeriphClkInit.PLLSAI1.PLLSAI1N = 24;
+    PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV7;
+    PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
+    PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
+    PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_48M2CLK;
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
         Error_Handler();
     }
-    /** Configure the main internal regulator output voltage 
-  */
+    /** Configure the main internal regulator output voltage
+     */
     if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK) {
         Error_Handler();
     }
 }
 
 /**
-  * @brief DAC1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief DAC1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_DAC1_Init(void) {
     /* USER CODE BEGIN DAC1_Init 0 */
 
@@ -323,14 +288,14 @@ static void MX_DAC1_Init(void) {
     /* USER CODE BEGIN DAC1_Init 1 */
 
     /* USER CODE END DAC1_Init 1 */
-    /** DAC Initialization 
-  */
+    /** DAC Initialization
+     */
     hdac1.Instance = DAC1;
     if (HAL_DAC_Init(&hdac1) != HAL_OK) {
         Error_Handler();
     }
-    /** DAC channel OUT1 config 
-  */
+    /** DAC channel OUT1 config
+     */
     sConfig.DAC_SampleAndHold = DAC_SAMPLEANDHOLD_DISABLE;
     sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
     sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
@@ -339,8 +304,8 @@ static void MX_DAC1_Init(void) {
     if (HAL_DAC_ConfigChannel(&hdac1, &sConfig, DAC_CHANNEL_1) != HAL_OK) {
         Error_Handler();
     }
-    /** DAC channel OUT2 config 
-  */
+    /** DAC channel OUT2 config
+     */
     sConfig.DAC_ConnectOnChipPeripheral = DAC_CHIPCONNECT_DISABLE;
     if (HAL_DAC_ConfigChannel(&hdac1, &sConfig, DAC_CHANNEL_2) != HAL_OK) {
         Error_Handler();
@@ -351,10 +316,10 @@ static void MX_DAC1_Init(void) {
 }
 
 /**
-  * @brief QUADSPI Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief QUADSPI Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_QUADSPI_Init(void) {
     /* USER CODE BEGIN QUADSPI_Init 0 */
 
@@ -380,10 +345,32 @@ static void MX_QUADSPI_Init(void) {
 }
 
 /**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief RNG Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_RNG_Init(void) {
+    /* USER CODE BEGIN RNG_Init 0 */
+
+    /* USER CODE END RNG_Init 0 */
+
+    /* USER CODE BEGIN RNG_Init 1 */
+
+    /* USER CODE END RNG_Init 1 */
+    hrng.Instance = RNG;
+    if (HAL_RNG_Init(&hrng) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN RNG_Init 2 */
+
+    /* USER CODE END RNG_Init 2 */
+}
+
+/**
+ * @brief TIM3 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM3_Init(void) {
     /* USER CODE BEGIN TIM3_Init 0 */
 
@@ -419,10 +406,10 @@ static void MX_TIM3_Init(void) {
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART1_UART_Init(void) {
     /* USER CODE BEGIN USART1_Init 0 */
 
@@ -450,10 +437,10 @@ static void MX_USART1_UART_Init(void) {
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void) {
     /* GPIO Ports Clock Enable */
     __HAL_RCC_GPIOA_CLK_ENABLE();
@@ -465,30 +452,14 @@ static void MX_GPIO_Init(void) {
 
 /* USER CODE END 4 */
 
-/* USER CODE BEGIN Header_StartDefaultTask */
 /**
-  * @brief  Function implementing the playSoundTask thread.
-  * @param  argument: Not used 
-  * @retval None
-  */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void const *argument) {
-    /* USER CODE BEGIN 5 */
-    /* Infinite loop */
-    for (;;) {
-        osDelay(1);
-    }
-    /* USER CODE END 5 */
-}
-
-/**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM17 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM17 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     /* USER CODE BEGIN Callback 0 */
 
@@ -502,9 +473,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void) {
     /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
@@ -514,12 +485,12 @@ void Error_Handler(void) {
 
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(char *file, uint32_t line) {
     /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
